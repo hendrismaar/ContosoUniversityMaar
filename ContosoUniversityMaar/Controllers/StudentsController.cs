28893fcc7ed8513db18eb8e531a6df6af11e069a
Opilane TTHK <opilane@tthk.ee>
2023-09-04 16:42:20 +0300

    stuff
    added studentscontroller and index view

---
+++ ContosoUniversityMaar/Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using ContosoUniversityMaar.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversityMaar.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public StudentsController(SchoolContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Students.ToListAsync());
+        }
+    }
+}
